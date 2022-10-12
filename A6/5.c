29495a335c4abbbf3dad765acb29306533e271e0
Saksham Bindal <saksham.bindal2004@gmail.com>
2022-10-12 15:55:34 +0530

    A6 Q5

--- A6/5.c
+++ A6/5.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 #include <omp.h>
-#define N 100000
-#define step 1000
+#define N 10
+#define step 3
 
 int main() {
-	int v1[N], v2[N];
-	for(int i=0; i<N; i++) v1[i] = v2[i] = 1;
+	int v1[N][N], v2[N][1], v3[N][1];
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) v1[i][j] = 1;
+		v2[i][0] = 1;
+		v3[i][0] = 0;
+	}
 	
-	int gsum = 0;
 	#pragma omp parallel
 	{
 		#pragma omp for
 		for(int i=0; i<N; i+=step) {
 			int start = i;
 			int end = start + step - 1;
-			if(end >= N) end = N - 1;
-			printf("Thread %d created task for %d to %d\n", omp_get_thread_num(), start, end);
+			if(end >= N) end = N;
+			
+			printf("Thread %d created task for rows %d to %d\n", omp_get_thread_num(), start, end);
 			#pragma omp task
 			{
-				printf("Thread %d performing the task for %d to %d\n", omp_get_thread_num(), start, end);
-				int lsum = 0;
-				for(int j=start; j<=end; j++) lsum += (v1[j] * v2[j]);
-				#pragma omp critical
-				gsum += lsum;
+				printf("Thread %d started task for rows %d to %d\n", omp_get_thread_num(), start, end);
+				for(int k=start; k<=end; k++) {
+					for(int j=0; j<N; j++) {
+						v3[k][0] += (v1[k][j] * v2[j][0]);
+					}
+				}
 			}
 		}
 	}
 	
-	printf("Result = %d\n", gsum);
+	printf("Result:\n");
+	for(int i=0; i<N; i++) printf("%d\n", v3[i][0]);
 	
 	return 0;
 }
